ffe9411f07a7d586b244c4197b8c39fa5c5490c6
Veny Cordova <venycordova@gmail.com>
2022-11-29 21:36:29 -0500

    managing documents

--- commands.js
+++ commands.js
@@ -84,13 +84,63 @@ db.bounties.insertMany(
 // MANAGE THE DATABASE
 // Queries
 // 1. Query for all bounties in the Grasslands
-
+db.bounties.find({ location: "Grasslands" })
+{ _id: ObjectId("6386b99c86f9cc175d450903"),
+  name: 'Thanoceros',
+  species: 'Rhinocers',
+  location: 'Grasslands',
+  wantedFor: 'Eating too much grass',
+  client: 'Songbird',
+  reward: 10000,
+  captured: false }
+{ _id: ObjectId("6386bd4986f9cc175d450905"),
+  name: 'Nebullama',
+  species: 'Llama',
+  location: 'Grasslands',
+  wantedFor: 'Drinking all the water from the ocean',
+  client: 'Songbird',
+  reward: 2500,
+  captured: false }
+{ _id: ObjectId("6386bd4986f9cc175d450907"),
+  name: 'Wrecking Crows',
+  species: 'Crow',
+  location: 'Grasslands',
+  wantedFor: 'Cawing too loudly',
+  client: 'Red wolf',
+  reward: 40000,
+  captured: false }
 // 2. Query for all bounties with a reward worth 10000 or more
-
+db.bounties.find( {reward: { $gte: 10000} })
+{ _id: ObjectId("6386b99c86f9cc175d450903"),
+  name: 'Thanoceros',
+  species: 'Rhinocers',
+  location: 'Grasslands',
+  wantedFor: 'Eating too much grass',
+  client: 'Songbird',
+  reward: 10000,
+  captured: false }
+{ _id: ObjectId("6386bd4986f9cc175d450907"),
+  name: 'Wrecking Crows',
+  species: 'Crow',
+  location: 'Grasslands',
+  wantedFor: 'Cawing too loudly',
+  client: 'Red wolf',
+  reward: 40000,
+  captured: false }
+{ _id: ObjectId("6386bd4986f9cc175d450908"),
+  name: 'Grandhog',
+  species: 'Groundhog',
+  location: 'Woodlands',
+  wantedFor: 'Not coming out of the hole on time and prolonging winter',
+  client: 'Songbird',
+  reward: 50000,
+  captured: false }
 // 3. Query for all bounties, but exclude the client attribute from being shown
-
+db.bounties.find({captured: false},{client:false})
 // 4. Query for a Groundhog in the Woodlands
-
+db.bounties.find( { species: "Groundhog" }, { location: "Woodlands" })
+{ _id: ObjectId("6386bd4986f9cc175d450908"),
+  location: 'Woodlands' }
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
 
